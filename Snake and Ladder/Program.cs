b511784a95eee1f7d7b49239964f03cc83dffadb
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-14 14:51:18 +0530

    UC3 Checking With Switch Case

--- Snake and Ladder/Program.cs
+++ Snake and Ladder/Program.cs
@@ -19,6 +19,25 @@
             Player1_numberOfRolls++;
 
             Console.WriteLine(" Player 1 Rolled: " + Player1_diceRoll);
+
+            int Player1_option = random.Next(0, 3);
+            switch (Player1_option)
+            {
+                case 0: // No Play
+                    Console.WriteLine("No Play");
+                    break;
+                case 1: // Ladder
+                    Player1_position += Player1_diceRoll;
+                    Console.WriteLine("Ladder!");
+                    break;
+                case 2: // Snake
+                    Player1_position -= Player1_diceRoll;
+                    Console.WriteLine("Snake!!");
+                    break;
+            }
+
+            Console.WriteLine("The position for Player 1 is : " + Player1_position);
+            Console.ReadLine();
         }
     }
 }
