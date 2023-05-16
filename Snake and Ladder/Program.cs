e7b0a3276a4af6c7f022ee7e4f9098a52f4ea9d4
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-16 07:30:38 +0530

    UC4

--- Snake and Ladder/Program.cs
+++ Snake and Ladder/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Snake and Ladder Problem ");
-   
+
             // player's start position:
 
             int Player1_position = 0;
@@ -15,29 +15,38 @@
 
             Random random = new Random();
 
-            int Player1_diceRoll = random.Next(1, 7);
-            Player1_numberOfRolls++;
-
-            Console.WriteLine(" Player 1 Rolled: " + Player1_diceRoll);
-
-            int Player1_option = random.Next(0, 3);
-            switch (Player1_option)
+            while (Player1_position < 100)
             {
-                case 0: // No Play
-                    Console.WriteLine("No Play");
-                    break;
-                case 1: // Ladder
-                    Player1_position += Player1_diceRoll;
-                    Console.WriteLine("Ladder!");
-                    break;
-                case 2: // Snake
-                    Player1_position -= Player1_diceRoll;
-                    Console.WriteLine("Snake!!");
-                    break;
-            }
 
-            Console.WriteLine("The position for Player 1 is : " + Player1_position);
-            Console.ReadLine();
+                int Player1_diceRoll = random.Next(1, 7);
+                Player1_numberOfRolls++;
+
+                Console.WriteLine(" Player 1 Rolled: " + Player1_diceRoll);
+
+                int Player1_option = random.Next(0, 3);
+                switch (Player1_option)
+                {
+                    case 0: // There is No Play
+                        Console.WriteLine(" There is No Play");
+                        break;
+
+                    case 1: // Ladder
+                        Player1_position += Player1_diceRoll;
+                        Console.WriteLine("Ladder!");
+                        break;
+
+                    case 2: // Snake
+                        Player1_position -= Player1_diceRoll;
+                        Console.WriteLine("Snake!!");
+                        break;
+                }
+
+                if (Player1_position < 0)
+                   { Player1_position = 0; }
+
+                    Console.WriteLine("The position for Player 1 is : " + Player1_position);
+                    Console.ReadLine();
+            }
         }
     }
 }
